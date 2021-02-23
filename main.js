d8f2d984d135b7972846e02d1a80bc03d6162db1
Erik Huang <erikhuangdrums@gmail.com>
2021-02-23 14:54:25 -0800

    done

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const modal = document.querySelector("#modal")
+const modalMsg = document.querySelector('#modal-message')
+const likeBtns = document.querySelectorAll('li.like')
 
 // Your JavaScript code goes here!
+window.addEventListener('load', () => {
+  modal.classList.add('hidden')
+  upvote(likeBtns)
+})
 
+const upvote = btns => {
+  for (let btn of likeBtns) {
+    btn.addEventListener('click', (e) => {
+      let heart = e.target.querySelector('span')
+      mimicServerCall('ランズのうすらボケ.org')
+        .then(json => {
+          if (heart.innerText === EMPTY_HEART) {
+            heart.innerText = FULL_HEART
+            heart.classList.add('activated-heart')
+          } else {
+            heart.innerText = EMPTY_HEART
+            heart.classList.remove('activated-heart')
+          }
+        })
+        .catch(err => {
+          modal.classList.remove('hidden')
+          modalMsg.innerText = err
+          setTimeout(() => {
+            modal.classList.add('hidden')
+          }, 5000)
+        })
+      
+      
+    })
+  }
+}
 
+// const mimicsServerCall = (e) => {
+  
+// }
 
 
 //------------------------------------------------------------------------------
